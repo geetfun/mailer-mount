2b72053f30559a76e2ee9a6c2f4c9faed2da8d95
Simon Chiu <simon@furvur.com>
2024-06-24 13:12:25 -0400

    Add generator

--- lib/generators/mailer_mount/install/install_generator.rb
+++ lib/generators/mailer_mount/install/install_generator.rb
@@ -1,7 +1,17 @@
+# frozen_string_literal: true
+
 class MailerMount::InstallGenerator < Rails::Generators::Base
   source_root File.expand_path("templates", __dir__)
 
   def create_migrations
     rails_command "railties:install:migrations FROM=mailer_mount", inline: true
+  end
+
+  def add_routes
+    route 'mount MailerMount::Engine => "/mailer_mount"'
+  end
+
+  def create_initializer_file
+    template "mailer_mount_initializer.rb", "config/initializers/mailer_mount.rb"
   end
 end
